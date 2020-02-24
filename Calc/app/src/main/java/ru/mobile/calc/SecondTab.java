6e66c606e101d79aee2210f53975e160932019eb
Skwardlow <gorskovdmitry@gmail.com>
2020-02-25 03:45:02 +0700

    added cats, pepe and some code

--- Calc/app/src/main/java/ru/mobile/calc/SecondTab.java
+++ Calc/app/src/main/java/ru/mobile/calc/SecondTab.java
@@ -1,14 +1,21 @@
 package ru.mobile.calc;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class SecondTab extends AppCompatActivity {
+
+public class SecondTab extends ListActivity {
+    int[] iconIds ={R.drawable.catmeme1,R.drawable.catmeme2,R.drawable.catmeme3,R.drawable.catmeme4,R.drawable.catmeme5};
+    String[] cats = {"Не очень довольный кот","Деловой кот","Грустный кот1","Грустный кот2","Грустный кот3"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_tab);
     }
+
 }
+
+
--- Calc/app/src/main/java/ru/mobile/calc/ThirdTab.java
+++ Calc/app/src/main/java/ru/mobile/calc/ThirdTab.java
@@ -2,13 +2,54 @@ package ru.mobile.calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class ThirdTab extends AppCompatActivity {
+    String[] cat = new String[]{"Не очень довольный кот","Деловой кот","Грустный кот1","Грустный кот2","Грустный кот3"};
+    int[] png = new int[]{R.drawable.catmeme1,R.drawable.catmeme2,R.drawable.catmeme3,R.drawable.catmeme4,R.drawable.catmeme5};
 
+
+    private TextView makerow(String s) {
+        TextView txt = new TextView(this);
+        txt.setText(s);
+        txt.setTypeface(Typeface.SERIF, Typeface.BOLD);
+        txt.setGravity(17);
+        return txt;
+    }
+    private ImageView makepng(int res) {
+        ImageView png = new ImageView(this);
+        png.setImageResource(res);
+        return png;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third_tab);
+        TableLayout table = new TableLayout(this);
+        TableRow[] rows = new TableRow[5];
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        rows[0] = new TableRow(this);
+        rows[0].setGravity(17);
+        rows[0].addView(makerow("Описание кота"));
+        TableRow.LayoutParams p = new TableRow.LayoutParams();
+        p.span = 2;
+        rows[0].addView(makerow("Фотография кота"), p);
+        table.addView(rows[0]);
+        for (int i = 0; i < 5; i++) {
+            rows[i] = new TableRow(this);
+            rows[i].setGravity(17);
+            rows[i].addView(makerow(this.cat[i]));
+            ImageView image = makepng(png[i]);
+            image.setMaxWidth(1);
+            image.setMaxHeight(1);
+            rows[i].addView(image);
+            table.addView(rows[i]);
+        }
+        setContentView(table);
     }
 }
