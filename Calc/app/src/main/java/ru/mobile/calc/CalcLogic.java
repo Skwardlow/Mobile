c27f7aa5cf574caf0d2a0e67e4d726aa289701ff
Skwardlow <gorskovdmitry@gmail.com>
2020-02-24 22:06:46 +0700

    added 2nd activity form

--- Calc/app/src/main/java/ru/mobile/calc/CalcLogic.java
+++
@@ -1,4 +0,0 @@
-package ru.mobile.calc;
-
-public class CalcLogic {
-}
--- Calc/app/src/main/java/ru/mobile/calc/MainActivity.java
+++ Calc/app/src/main/java/ru/mobile/calc/MainActivity.java
@@ -1,15 +1,32 @@
 package ru.mobile.calc;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button labOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calc);
+
+        labOne = (Button) findViewById(R.id.button1);
+        labOne.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(this, Calc.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
